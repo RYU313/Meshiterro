60bca82cce6ed4cb0c86a89a0523a3df0c9b9000
RYU313 <umaryu313@gmail.com>
2021-09-20 15:57:11 +0000

    update

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,14 +4,25 @@ class PostImagesController < ApplicationController
   end
 
   def create
+    @post_image = PostImage.new
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.new
   end
 
   def destroy
+  end
+
+  private
+  def post_image_params
+    params.require(:post_image).premit(:shop_name, :image, :caption)
   end
 end
