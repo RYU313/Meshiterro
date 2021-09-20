57128645e950bd079882f2e2b2b372a1085c6b82
RYU313 <umaryu313@gmail.com>
2021-09-20 16:09:04 +0000

    update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '30f0b629b672d2f4e11d9ff007731719279b37d67f3b08f6dd24219e40c30cc4d351fc13e7a925e4038ffd0e6b35e93ce0dd3d2d2ac99696a1f857e96ca22a68'
